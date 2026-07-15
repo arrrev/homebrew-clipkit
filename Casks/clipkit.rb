0caa03da55ae8f9995ebce8386ba342a73fd9dc6
Arevik Martirosyan <armarty5@gmail.com>
2026-07-15 21:08:12 +0400

    clipkit 2.1.0

--- Casks/clipkit.rb
+++ Casks/clipkit.rb
@@ -1,6 +1,6 @@
 cask "clipkit" do
-  version "2.0.9"
-  sha256 "5c82ce034d7c2f6702f18ac176675c30c42ad3d7da66b7a869e199d71de14194"
+  version "2.1.0"
+  sha256 "3ff93fc50ef1842724cd32148f6567beccc7edaae0c548ddcdf2da3ca7e3512a"
 
   url "https://github.com/arrrev/clipkit/releases/download/v#{version}/ClipKit-#{version}.zip"
   name "ClipKit"
